bb7f99e1f2a8d3fd14735a17e6eb89318233d864
Greg Chappelow <greg.chappelow@gmail.com>
2024-08-13 23:05:40 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const { engine } = require('express-handlebars');
+const path = require('path');
+
+
+const PORT = process.env.PORT || 5000;
+
+// Set Handlebars Middleware
+app.engine('handlebars', engine({ extname: '.hbs', defaultLayout: "main.handlebars"}));
+app.set('view engine', 'handlebars');
+
+// Set handlebar routes
+app.get('/', function (req, res) {
+	res.render('home', {
+		stuff:  "This is stuff..."
+	});
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT))
